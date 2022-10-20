4c7abcaf9ad8ecefb229579c758933d4bbcc4701
DanielSan <kaiserkey2@gmail.com>
2022-10-20 20:02:47 -0300

    trabajndo con nodejs

--- clase_5.js
+++ clase_5.js
@@ -1,1 +1,18 @@
 'use strict'
+const fs = require('fs')
+
+//trabajando con streams - escritura y lectura de archivos
+let readStream = fs.createReadStream('assets/nombres.txt')
+let writeStream = fs.createWriteStream('assets/nombres_copia.txt')
+
+readStream.pipe(writeStream)
+
+readStream.on('data', function(chunk){
+    console.log('Datos leidos -> ' + chunk.length + ' Caracteres')
+})
+
+readStream.on('end', function(){
+    console.log('Se ha terminado de leer el archivo...')
+})
+
+
