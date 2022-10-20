0473b3fc280a515bf3727097405d270c44377e19
DanielSan <kaiserkey2@gmail.com>
2022-10-20 18:32:42 -0300

    nodejs clases

--- clase_3.js
+++ clase_3.js
@@ -1,1 +1,24 @@
 'use strict'
+
+/* var http = require('http');
+
+http.createServer(function (request, response){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World!\n');
+}).listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/'); */
+
+var http = require('http');
+
+function webServer(req, res){
+    res
+    .writeHead(200, {'Content-Type': 'text/html'})
+    .end('<h1>Hello World!</h1>');
+}
+
+http
+.createServer(webServer)
+.listen(3000, 'localhost');
+
+console.log('Servidor corriendo en: http://localhost:300/');
