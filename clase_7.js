21e4bc05e682407e1a4c58c2239795be7e8ac531
DanielSan <kaiserkey2@gmail.com>
2022-10-21 01:15:41 -0300

    asdasd

--- clase_7.js
+++ clase_7.js
@@ -1,1 +1,18 @@
 'use strict'
+//noidejs eventos
+const EventEmitter = require('events').EventEmitter,
+        pub = new EventEmitter()
+
+pub
+    .on('myEvent', function(message){
+        console.log(message)
+    })
+    .once('myEvent', function (message){
+        console.log('Se emite el primer evento...')
+    })
+
+pub.emit('myEvent', 'Soy un emisor de eventos')
+pub.emit('myEvent', 'Volviendo a emitir el evento')
+//pub.removeAllListeners('myEvent')
+pub.emit('myEvent', 'Volviendo a emitir el evento por tercera vez')
+
--- clase_8.js
+++ clase_8.js
@@ -1,1 +1,38 @@
 'use strict'
+
+//emisor de eventos con herencia
+const EventEmitter = require('events').EventEmitter
+class Clock{
+
+    mostrarHora(){
+        let pub = new EventEmitter()
+        
+        let hora = ()=>{
+            let date = new Date(),
+            hrs = date.getHours(),
+            min = date.getMinutes(),
+            sec = date.getSeconds()
+            
+
+            if (hrs < 10) hrs = "0" + hrs;
+            if (min < 10) min = "0" + min;
+            if (sec < 10) sec = "0" + sec;
+
+            let msg = hrs+':'+min+':'+sec
+
+            console.log(msg)
+        }
+
+        pub.on('tictac', function(){ hora() })
+
+        setInterval(function (){
+            pub.emit('tictac')
+        }, 1000);
+    }
+
+}
+
+let reloj = new Clock()
+
+reloj.mostrarHora()
+
