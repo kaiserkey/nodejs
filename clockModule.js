65bdd8c5087c09937a405898116722e0810e395e
DanielSan <kaiserkey2@gmail.com>
2022-10-21 13:52:21 -0300

    clock

--- clockModule.js
+++ clockModule.js
@@ -8,32 +8,26 @@ module.exports = class Clock{
         
         let hora = ()=>{
             let date = new Date(),
-            addZero = (num)=>{
+            addZero = (num)=>
+            {
                 return (num<10) ? '0'+num : num
             },
-            rango = ()=>{
-                return (date.getHours()<12) ? ' AM' : ' PM'
-
+            rango = ()=>
+            {
+                return (date.getHours()<12) ? 'AM' : 'PM'
             },
             hrs = addZero(date.getHours()),
             min = addZero(date.getMinutes()),
             sec = addZero(date.getSeconds())
 
-            let msg = hrs+':'+min+':'+sec+rango()
+            let msg = `${hrs}:${min}:${sec} ${rango()}`
 
             console.log(msg)
         }
 
-        pub.on('tictac', function(){ hora() })
+        pub.on('tictac', ()=>{ hora() })
 
-        setInterval(function (){
-            pub.emit('tictac')
-        }, 1000);
+        setInterval( ()=>{ pub.emit('tictac') }, 1000);
     }
 
 }
-
-
-
-
-/*  module.exports.Clock = new Clock() */
